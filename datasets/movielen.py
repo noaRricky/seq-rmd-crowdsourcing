ca2252eb9adbe048b31e431f74a7bb1e91ce9fd6
NoaRicky <480582562@qq.com>
2019-06-21 17:05:48 +0800

    add python env file

---
+++ datasets/movielen.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+
+class Movielen10K(object):
+    def __init__(self, data_path: Path) -> None:
+        pass
+
+
+class Movielen10M(object):
+    def __init__(self, data_path: Path) -> None:
+        pass
