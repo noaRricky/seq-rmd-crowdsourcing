bd1777aed303fafb5aadc055cdc44fd27c0cb6a0
Ricky <480582562@qq.com>
2019-06-25 16:56:15 +0800

    implement item popularity model

--- models/item_popularity.py
+++ models/item_popularity.py
@@ -12,23 +12,33 @@ class ItemPopularity(object):
 
     def fit(self, df: pd.DataFrame) -> None:
         item_name = self.item_name
-        self.items_pop = df[item_name].sort_values(
+        self.items_pop = df[item_name].value_counts(
             ascending=False).index[:self.topk]
 
     def evalute(self, test_df: pd.DataFrame) -> None:
         test_series = test_df[self.item_name]
         self.percision = self._precision(test_series, self.items_pop)
+        self.mean_recirocal_rank = self._mean_recirocal_rank(
+            test_series, self.items_pop)
 
     def _precision(self, test_data: pd.Series,
                    rmd_data: pd.Int64Index) -> float:
-        right_num = test_data.sort_values()[rmd_data].sum()
+        count_series = test_data.value_counts()
+        right_num = 0
+        for each_item in rmd_data:
+            if each_item in count_series.index:
+                right_num += count_series[each_item]
         record_num = test_data.size
         return right_num / record_num
 
     def _mean_recirocal_rank(self, test_data: pd.Series,
                              rmd_data: pd.Int64Index) -> float:
         record_num = test_data.size
-        right_array = test_data.value_counts()[rmd_data].to_numpy()
+        count_series = test_data.value_counts()
+        right_array = np.zeros(shape=(self.topk))
+        for index, each_item in enumerate(rmd_data):
+            if each_item in count_series.index:
+                right_array[index] = count_series[each_item]
         mmr_array = right_array / np.arange(start=1, stop=self.topk + 1)
         mmr_rate = np.sum(mmr_array)
         return mmr_rate / record_num
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,3 +1,4 @@
+from typing import Dict
 from pathlib import Path
 from unittest import TestCase
 
@@ -9,6 +10,18 @@ from models import SimpleModel, ItemPopularity
 MOVIE10K_PATH = Path('inputs/ml-100k/u.data')
 
 
+def split_train_test(df: pd.DataFrame) -> Dict[str, pd.DataFrame]:
+    df.sort_values(by=['timestamp'], inplace=True)
+    last_mask = df.duplicated(subset=['userId'], keep='last')
+
+    train_df: pd.DataFrame = df[last_mask]
+    test_df: pd.DataFrame = df[~last_mask]
+
+    train_df.sort_values(by=['userId', 'timestamp'], inplace=True)
+    test_df.sort_values(by=['userId', 'timestamp'], inplace=True)
+    return {'train': train_df, 'test': test_df}
+
+
 class TestModel(TestCase):
     def test_simple_model(self):
         dataset = Movielen10K(MOVIE10K_PATH)
@@ -18,7 +31,10 @@ class TestModel(TestCase):
         model = SimpleModel(vocab_dict, emb_dict, num_classes)
 
     def test_item_pop(self):
-        data = pd.DataFrame(data={'item': [1, 2, 1, 2, 3, 3, 1]})
-        model = ItemPopularity(item_name='item')
-        model.fit(data)
-        print(model.items_pop)
+        df = pd.read_csv('inputs/ml-100k/u.data',
+                         sep='\t',
+                         names=['userId', 'movieId', 'rating', 'timestamp'],
+                         engine='python')
+        model = ItemPopularity(item_name='movieId', topk=20)
+        model.fit(df)
+        model.evalute(df)
