a2edc7c885c61cb298d74505d0f41dfb32d65f33
Ricky <480582562@qq.com>
2019-06-25 15:32:28 +0800

    add itempop evalution methods

--- models/item_popularity.py
+++ models/item_popularity.py
@@ -1,16 +1,34 @@
+import numpy as np
 import pandas as pd
 
 
 class ItemPopularity(object):
-    def __init__(self, item_name: str, topk: int = 10) -> None:
+    def __init__(self, item_name: str, topk: int = 20) -> None:
         self.item_name = item_name
         self.items_pop: pd.DataFrame = None
         self.topk = topk
+        self.percision: float
+        self.mean_recirocal_rank: float
 
-    def fit(self, data: pd.DataFrame) -> None:
+    def fit(self, df: pd.DataFrame) -> None:
         item_name = self.item_name
-        items_pop = data[[item_name]].groupby(by=[item_name]).size()
-        self.items_pop = items_pop.sort_values(ascending=False)[:self.topk]
+        self.items_pop = df[item_name].sort_values(
+            ascending=False).index[:self.topk]
 
-    def evalute(self, test_data) -> None:
-        pass
+    def evalute(self, test_df: pd.DataFrame) -> None:
+        test_series = test_df[self.item_name]
+        self.percision = self._precision(test_series, self.items_pop)
+
+    def _precision(self, test_data: pd.Series,
+                   rmd_data: pd.Int64Index) -> float:
+        right_num = test_data.sort_values()[rmd_data].sum()
+        record_num = test_data.size
+        return right_num / record_num
+
+    def _mean_recirocal_rank(self, test_data: pd.Series,
+                             rmd_data: pd.Int64Index) -> float:
+        record_num = test_data.size
+        right_array = test_data.value_counts()[rmd_data].to_numpy()
+        mmr_array = right_array / np.arange(start=1, stop=self.topk + 1)
+        mmr_rate = np.sum(mmr_array)
+        return mmr_rate / record_num
